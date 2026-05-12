114b262c3a1b5982131b87cce7af29f6c9e25c34
EtiennePagec <121800083+EtiennePagec@users.noreply.github.com>
2026-05-12 17:52:12 +0200

    upgrade ESLint tooling

---
+++ eslint.config.cjs
@@ -0,0 +1,64 @@
+const js = require('@eslint/js')
+const globals = require('globals')
+const tsPlugin = require('@typescript-eslint/eslint-plugin')
+const tsParser = require('@typescript-eslint/parser')
+
+module.exports = [
+  {
+    ignores: [
+      'build/',
+      'node_modules/',
+      '__tests__/',
+      '*.js'
+    ]
+  },
+  js.configs.recommended,
+  {
+    files: ['src/**/*.{ts,tsx}', 'tests/**/*.ts'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      parser: tsParser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2022,
+        ...globals.jest
+      }
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      'indent': ['warn', 2, { 'SwitchCase': 1 }],
+      'eqeqeq': 'off',
+      'prefer-const': 'off',
+      'no-undef': 'off',
+      'no-redeclare': 'off',
+      'no-case-declarations': 'off',
+      'no-useless-assignment': 'off',
+      'space-before-function-paren': ['error', {
+        'anonymous': 'always',
+        'named': 'never',
+        'asyncArrow': 'ignore'
+      }],
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-expressions': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-var-requires': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/no-empty-object-type': 'off',
+      '@typescript-eslint/no-unsafe-function-type': 'off',
+      '@typescript-eslint/no-wrapper-object-types': 'off'
+    }
+  }
+]
