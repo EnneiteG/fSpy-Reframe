85646900d53649527904289e6a490c459eeb3c8c
EtiennePagec <121800083+EtiennePagec@users.noreply.github.com>
2026-05-13 11:13:19 +0200

    upgrade Jest to version 30

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,14 @@
 /** @type {import('jest').Config} */
 module.exports = {
-  preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/tests'],
   testMatch: ['**/*tests.ts', '**/*specs.ts', '**/*_tests.ts'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      diagnostics: {
+        ignoreCodes: [151002]
+      }
+    }]
+  },
   reporters: ['default', 'jest-junit']
 }
